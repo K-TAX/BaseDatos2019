cc9d140b3919867e87905a5fbe94ec06d2ac065f
Ivan Peters <ivan.peters.v@gmail.com>
2019-03-13 20:31:08 -0300

    ASignacion de Tarea para ahora

--- ejemplosQuery.sql
+++ ejemplosQuery.sql
@@ -3,4 +3,9 @@ CREATE TABLE Alumno(
     Rut INT PRIMARY KEY,
     Nombre NVARCHAR(50) NOT NULL,
     Sexo TINYINT DEFAULT 0,
+    -- 0 == Mujer
+    -- 1 == Hombre
+    -- 3 == Desconcido
 );
+-- Hola soy {su nombre } y debo crear 5 insert into
+--insert into Alumno VALUES (16111,'Juanito Perez', 0)
