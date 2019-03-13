264f55274d1c46d8859fbdd05ef0d37c1aedd86c
Your Name <you@example.com>
2019-03-13 20:48:15 -0300

    Victor Hott Tarea

--- ejemplosQuery.sql
+++ ejemplosQuery.sql
@@ -9,3 +9,10 @@ CREATE TABLE Alumno(
 );
 -- Hola soy {su nombre } y debo crear 5 insert into
 --insert into Alumno VALUES (16111,'Juanito Perez', 0)
+
+insert into Alumno(Rut,Nombre,Sexo) Values(182386162,'Victor Hott',1)
+insert into Alumno(Rut,Nombre,Sexo) Values(172971059,'Cyntia Aravena',0)
+insert into Alumno(Rut,Nombre,Sexo) Values(71129705,'Victor Hott Tejeda',1)
+insert into Alumno(Rut,Nombre,Sexo) Values(165879457,'Paola Hott',0)
+insert into Alumno(Rut,Nombre,Sexo) Values(89240719,'Angelica Diaz',0)
+
