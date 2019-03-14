b81327f2ab155baf8a73700610adbd5ed8a4d269
Alex Sotomayor <alex_sotomayor@outlook.es>
2019-03-13 21:15:06 -0300

    commit alex sotomayor

--- ejemplosQuery.sql
+++ ejemplosQuery.sql
@@ -9,3 +9,8 @@ CREATE TABLE Alumno(
 );
 -- Hola soy {su nombre } y debo crear 5 insert into
 --insert into Alumno VALUES (16111,'Juanito Perez', 0)
+insert into Alumno value(18.493.000-5,'Jose Vargas',1)
+insert into Alumno value(18.487.995-8,'Alex Sotomayor',1)
+insert into Alumno value(17.849.777-8,'Anita Atero',0)
+insert into Alumno value(16.777.888-7,'Juanita Vera',1)
+insert into Alumno value(19.666.666-6,'Marcos Brabo',1)
