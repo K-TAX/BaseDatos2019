b6ab365046c902c985b782adbebfdb5612cb6a1a
Evelynhernandeza <evelyn.hernandez3036@gmail.com>
2019-03-13 21:08:36 -0300

    jajajajajaj xdddd soy la que le faltó el completo

--- ejemplosQuery.sql
+++ ejemplosQuery.sql
@@ -9,3 +9,8 @@ CREATE TABLE Alumno(
 );
 -- Hola soy {su nombre } y debo crear 5 insert into
 --insert into Alumno VALUES (16111,'Juanito Perez', 0)
+Insert into Alumno(Rut,Nombre,Sexo) values (152967365,'Evelyn Hernandez',0);
+Insert into Alumno(Rut,Nombre,Sexo) values (152967366,'Francisco Panichini',1);
+Insert into Alumno(Rut,Nombre,Sexo) values (152967367,'Arlette Duran',0);
+Insert into Alumno(Rut,Nombre,Sexo) values (152967368,'Saul Delgado',1);
+Insert into Alumno(Rut,Nombre,Sexo) values (152967369,'Agustin Carrasco',1);
